7a312898b35410c7df4a8c8e481974ce07a95720
Richard Newman <rnewman@twinql.com>
2021-09-18 18:03:13 -0700

    Add stub WebExtension using Svelte.

---
+++ webextension/components/App.svelte
@@ -0,0 +1,23 @@
+<svelte:options tag={null}/>
+<script lang="ts">
+  // import browser from "webextension-polyfill";
+  console.info('yyy');
+</script>
+
+<main>
+  <p>Hello, world</p>
+</main>
+
+<style>
+  main {
+    padding: 1em;
+    max-width: 640px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
